8f634a10d02b433c47cc67ad8bd9fc84e6af8283
nachoria <nachito.sk8@hotmail.com>
2019-07-12 20:38:20 -0300

    .

--- src/com/emmettbrown/controles/Teclado.java
+++ src/com/emmettbrown/controles/Teclado.java
@@ -22,7 +22,7 @@ public class Teclado implements KeyListener {
 	@Override
 	public void keyPressed(KeyEvent e) {
 		
-//		if (mov == Movimientos.NULL) {
+		if (mov == Movimientos.NULL) {
 			switch (e.getKeyCode()) {
 			case KeyEvent.VK_DOWN:
 					this.mov = Movimientos.ABAJO;
@@ -55,56 +55,12 @@ public class Teclado implements KeyListener {
 			default:
 				break;
 			}
-//		}
-
-//		if (cliente.getBomber() != null && cliente.getBomber().verSiEsVisible()) {
-//			if (key == KeyEvent.VK_ESCAPE) {
-//				JVentanaInicial ventAct = new JVentanaInicial(cliente);
-//				ventAct.setVisible(true);
-//				cliente.getVentanaGrafica().dispose();
-//			}
-//			if (key == KeyEvent.VK_RIGHT) {
-//				System.out.println("no solte");
-//				if (mov == Movimientos.NULL) {
-//					this.cliente.enviarMsg(new MsgMover(Movimientos.DERECHA));
-//					this.mov = Movimientos.DERECHA;
-//				}
-//				
-//			}
-//			if (key == KeyEvent.VK_LEFT) {
-//				if (mov == Movimientos.NULL) {
-//					this.cliente.enviarMsg(new MsgMover(Movimientos.IZQUIERDA));
-//					this.mov = Movimientos.IZQUIERDA;
-//				}
-////				this.cliente.enviarMsg(new MsgMover(Movimientos.IZQUIERDA));
-//			}
-//			if (key == KeyEvent.VK_UP) {
-//				if (mov == Movimientos.NULL) {
-//					this.cliente.enviarMsg(new MsgMover(Movimientos.ARRIBA));
-//					this.mov = Movimientos.ARRIBA;
-//				}
-////				this.cliente.enviarMsg(new MsgMover(Movimientos.ARRIBA));
-//			}
-//			if (key == KeyEvent.VK_DOWN) {
-//				
-//				if (mov == Movimientos.NULL) {
-//					this.mov = Movimientos.ABAJO;
-//					this.cliente.enviarMsg(new MsgMover(Movimientos.ABAJO));
-//					
-//				}
-////				this.cliente.enviarMsg(new MsgMover(Movimientos.ABAJO));
-//			}
-//			if (key == KeyEvent.VK_L) {
-//				if(this.cliente.getBomber().cantBombasAct() < DefConst.CANTBOMBASMAX)
-//					this.cliente.enviarMsg(new MsgMover(Movimientos.BOMBA));
-//			}
-//		}
+		}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 
-//		int key = e.getKeyCode();
 
 		switch (e.getKeyCode()) {
 		case KeyEvent.VK_DOWN:
@@ -132,34 +88,6 @@ public class Teclado implements KeyListener {
 		default:
 			break;
 		}
-		
-		
-//		if (cliente.getBomber() != null && cliente.getBomber().verSiEsVisible()) {
-//			if (key == KeyEvent.VK_ESCAPE) {
-//				//Agregar comando para salir
-//			}
-//			if (key == KeyEvent.VK_RIGHT) {
-//				this.cliente.enviarMsg(new MsgMover(Movimientos.NULL));
-//				this.mov = Movimientos.NULL;
-//			}
-//			if (key == KeyEvent.VK_LEFT) {
-//				this.cliente.enviarMsg(new MsgMover(Movimientos.NULL));
-//				this.mov = Movimientos.NULL;
-//			}
-//			if (key == KeyEvent.VK_UP) {
-//				this.cliente.enviarMsg(new MsgMover(Movimientos.NULL));
-//				this.mov = Movimientos.NULL;
-//			}
-//			if (key == KeyEvent.VK_DOWN) {
-//				this.cliente.enviarMsg(new MsgMover(Movimientos.NULL));
-//				this.mov = Movimientos.NULL;
-//			}
-//			
-//			if (key == KeyEvent.VK_L ) {
-//				this.cliente.enviarMsg(new MsgMover(Movimientos.NULL));
-//				
-//			}
-//		}
 	}
 	
 	@Override
